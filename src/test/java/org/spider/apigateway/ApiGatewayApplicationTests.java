bcc232a7d2c4dc2ab7e7dc987143c4a25ac3fe62
Malaya2184 <malayakumarswain8@gmail.com>
2024-08-16 14:00:27 +0530

    init gateway

---
+++ src/test/java/org/spider/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spider.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
